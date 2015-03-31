d7c861eebe4918723c96cd5433af047ac19bab90
emanon001 <emanon001@gmail.com>
2015-03-31 17:31:52 +0900

    first commit

---
+++ Meowziq/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Meowziq
+//
+//  Created by Seiji Kohyama on 2015/03/31.
+//  Copyright (c) 2015年 emanon001. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ README.md
@@ -0,0 +1,3 @@
+# :scream_cat: Meowziq for iOS :scream_cat:
+
+Meowziq client for iOS
