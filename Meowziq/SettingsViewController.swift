72edfde2b995542dd7b0ca16a9f95b606f616286
emanon001 <emanon001@gmail.com>
2015-04-07 18:30:07 +0900

    APIサーバーURLを設定できるよう修正

---
+++ Meowziq/SettingsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SettingsViewController.swift
+//  Meowziq
+//
+//  Created by Seiji Kohyama on 2015/04/07.
+//  Copyright (c) 2015年 FaithCreates Inc. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+    
+    @IBOutlet weak var apiServerURLLabel: UILabel!
+    @IBOutlet weak var apiServerURLTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        apiServerURLTextField.text = APIClient.apiServerURL
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func saveAPIServerURL(sender: AnyObject) {
+        APIClient.apiServerURL = apiServerURLTextField.text
+    }
+}
